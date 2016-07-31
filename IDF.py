959856cb811b79cfc96f93cff132669424bcde1f
Aaron Yang <ycr_aaron@foxmail.com>
2016-08-01 04:52:21 +0800

    HAC

--- IDF.py
+++ IDF.py
@@ -1,11 +1,13 @@
 from __future__ import division
 from stop_words import get_stop_words
 import string
+import math
+import operator
 
 class IDF:
     def __init__(self, str_1, str_2, corpus):
-        self.corpus = corpus
-
+        self.list_corpus = corpus
+        self.stop_words = get_stop_words('en')
         # remove punctuation
         str_1 = str_1.translate(string.maketrans("", ""), string.punctuation)
         str_2 = str_2.translate(string.maketrans("", ""), string.punctuation)
@@ -14,10 +16,38 @@ class IDF:
         self.str_1 = self.tokenize(str_1)
         self.str_2 = self.tokenize(str_2)
 
+    def intersect(self):
+        return [i for i in self.str_1 if i in self.str_2]
+
+    def union(self):
+        return list(self.str_1 + self.str_2)
+
     # tokenize str and remove stop words
     def tokenize(self, item):
-        item = item.lower().split(" ")
+        item = item.split(" ")
         for token in item:
             if token in self.stop_words:
                 item.remove(token)
         return item
+
+    def cal_overlap(self):
+        list_intersect = self.intersect()
+        list_union = self.union()
+        freq_inter = 0.0
+        freq_union = 0.0
+        for item in list_intersect:
+            matches_intersect = [x for x in self.list_corpus if item == x]
+            freq_inter += operator.pow(math.log10(len(matches_intersect) + 1), -1)
+
+        for item in list_union:
+            matches_union = [x for x in self.list_corpus if item == x]
+            freq_union += operator.pow(math.log10(len(matches_intersect) + 1), -1)
+
+        idf_overlap = freq_inter / freq_union
+        return idf_overlap
+        # print matches_intersect, freq_inter
+        # print matches_union, freq_union
+        #
+        # print "intersect", list_intersect
+        # print "union", list_union
+        # print "corpus", self.list_corpus
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from stop_words import get_stop_words
+
 import jaccard
 import jellyfish
 import MySQLdb
-from stop_words import get_stop_words
+import IDF
 
 DB = "database"
 DB_HOST = "localhost"
@@ -10,11 +12,12 @@ DB_NAME = "linked_reverb"
 DB_USER = "root"
 DB_PWD = "12345"
 
+cachedStopWords = get_stop_words("en")
 
 def get_corpus():
     db_conn = MySQLdb.connect(host="localhost", port=8889, db="linked_reverb", user="root", passwd="root")
     cursor = db_conn.cursor()
-    cursor.execute("select argument1, argument2 from linked_entity80_a where link_score > 150")
+    cursor.execute("select argument1, argument2 from linked_entity80_a")
 
     ls_result = []
     ls_corpus = []
@@ -43,16 +46,46 @@ def get_corpus():
                 # ls_corpus.append(ls_result[i][1].split(" "))
 
     db_conn.close()
-
     return ls_corpus
 
-# Jaron distance
-jarowinker = jellyfish.jaro_winkler(u'MARTHA tmp', u'MARTHA tmp')
-jaro = jellyfish.jaro_distance(u'MARTHA', u'MARTHA')
-# Jaccard distance
-jaccard = jaccard.Jaccard("will be a common year starting on Thursday of the Gregorian calendar", "is a common year starting on Thursday of the Gregorian calendar")
-similarity = jaccard.similarity()
-distance = jaccard.distance()
-# IDF
-list_corpus = get_corpus()
+def get_subject():
+    db_conn = MySQLdb.connect(host="localhost", port=8889, db="linked_reverb", user="root", passwd="root")
+    cursor = db_conn.cursor()
+    cursor.execute("select argument1 from linked_entity80_a")
 
+    ls_result = []
+    ls_subject = []
+
+    row_count = int(cursor.rowcount)
+    for i in range(0, row_count):
+        row = cursor.fetchone()
+        ls_result.append(row)
+
+    for i in range(0, len(ls_result)):
+        print ls_result[i][0].split()
+        subj = ' '.join([word for word in str(ls_result[i][0]).split() if word not in cachedStopWords])
+        #ls_result[i][0] = ' '.join([word for word in str(ls_result[i][0]).split() if word not in cachedStopWords])
+        ls_subject.append(subj)
+
+    print ls_subject
+                #
+                # ls_corpus.append(ls_result[i][0].split(" "))
+                # ls_corpus.append(ls_result[i][1].split(" "))
+
+    db_conn.close()
+    return ls_subject
+
+
+# # Jaron distance
+# jarowinker = jellyfish.jaro_winkler(u'MARTHA tmp', u'MARTHA tmp')
+# jaro = jellyfish.jaro_distance(u'MARTHA', u'MARTHA')
+# # Jaccard distance
+# jaccard = jaccard.Jaccard("will be a common year starting on Thursday of the Gregorian calendar", "is a common year starting on Thursday of the Gregorian calendar")
+# similarity = jaccard.similarity()
+# distance = jaccard.distance()
+# # IDF
+# list_corpus = get_corpus()
+#
+# idf = IDF.IDF("Sushi Mutzig", "Sushi marinara Mutzig", list_corpus)
+#
+# #print idf.cal_overlap()
